22ed2e8d7fe968ed2009037e209b6d4bd3deb6a4
Michael Nt <midall@users.noreply.github.com>
2016-03-12 17:22:49 +0000

    Add menu

--- src/bingo/Bingo.java
+++ src/bingo/Bingo.java
@@ -1,16 +1,39 @@
-/* TRUNK 1*/
+/* TRUNK */
 package bingo;
 
+import java.util.Scanner;
 import bingo.*;
 
 public class Bingo {
 
     public static void main(String[] args) {
-        System.out.println("Please Select: ");
-        System.out.println("1. new game ");
+        int choice = 0;
+        Scanner input = new Scanner(System.in);
         
-        
-        
-    }
+        do {
+            System.out.println("Please Select: ");
+            System.out.println("1. New player ");
+            System.out.println("0. Exit ");
+
+
+            choice = input.nextInt();
+
+            if(choice == 1) {
+                
+
+            } else if(choice == 0) {
+                System.out.println("Bye bye!");
+                
+            } else {
+                System.out.println("Please try again");
+                
+            }
+            
+            //end while
+        }while(choice != 0);
     
+        //end main
+    }
+ 
+    //end class
 }
