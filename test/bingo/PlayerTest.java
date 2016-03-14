2e633ba3098d6325f5cd3548f83f1c31bd26d22d
Michael Nt <midall@users.noreply.github.com>
2016-03-14 10:48:54 +0000

    Add new tests

--- test/bingo/PlayerTest.java
+++ test/bingo/PlayerTest.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.*;
  * @author Michalis
  */
 public class PlayerTest {
+    private Player pl1;
     
     public PlayerTest() {
     }
@@ -26,6 +27,7 @@ public class PlayerTest {
     
     @Before
     public void setUp() {
+        pl1 = new Player();
     }
     
     @After
@@ -34,6 +36,61 @@ public class PlayerTest {
 
     @Test
     public void Player() {
+        
+    }
+
+    /**
+     * Test of getName method, of class Player.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("getName");
+        String expResult = null;
+        String result = pl1.getName();
+        assertEquals(expResult, result);
+        
+    }
+    
+    @Test
+    public void testGetName1() {
+        System.out.println("getName - 1");
+        pl1.setName("Nick");
+        String expResult = "Nick";
+        String result = pl1.getName();
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of setName method, of class Player.
+     */
+    @Test
+    public void testSetName() {
+        System.out.println("setName");
+        //pl1.setName(null);
+        String expResult = null;
+        String result = pl1.getName();
+        assertEquals(expResult, result);
+        
+    }
+    
+    @Test
+    public void testSetName1() {
+        System.out.println("setName - 1");
+        pl1.setName("");
+        String expResult = "";
+        String result = pl1.getName();
+        assertEquals(expResult, result);
+        
+    }
+    
+    @Test
+    public void testSetName2() {
+        System.out.println("setName - 2");
+        pl1.setName("Nick");
+        String expResult = "Nick";
+        String result = pl1.getName();
+        assertEquals(expResult, result);
         
     }
     
